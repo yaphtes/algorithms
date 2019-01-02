325b16f117521d88fbd19b10fad29785e275f3ce
Konstantin Otpushchennikov <otpushchennikov.ks@gmail.com>
2019-01-02 17:13:24 +0300

    Add singleton pattern

---
+++ patterns/creational/singleton.js
@@ -0,0 +1,16 @@
+// module
+class Singleton {
+  async constructor(params) {
+    if (!Singleton.instance) {
+      Singleton.instance = this;
+    }
+
+    // initialization
+    this.url = params.url;
+
+    return Singleton.instance;
+  }
+}
+
+// client
+const instance = Singleton.getInstance({ url: 'http://example.com' });
