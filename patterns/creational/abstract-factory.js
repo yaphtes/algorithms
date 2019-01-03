628bc49ef34283ddd7773e0b523a065ccc20857b
Konstantin Otpushchennikov <otpushchennikov.ks@gmail.com>
2019-01-03 19:28:00 +0300

    Add abstract factory pattern

---
+++ patterns/creational/abstract-factory.js
@@ -0,0 +1,76 @@
+// abstract factory module
+class DrinkAbstractFactory {
+  static createFactory(type) {
+    if (!type) {
+      throw Error('type should not be empty');
+    }
+
+    const Factory = AbstractFactory.factories[type];
+    if (typeof factory != 'function') {
+      throw new Error('can`t find factory');
+    }
+
+    return Factory;
+  }
+
+  static factories = {
+    CoffeeFactory,
+    TeaFactory,
+  };
+}
+
+
+// CoffeeFactory module
+class CoffeeFactory {
+  static create(type, params) {
+    if (!type) {
+      throw new Error('type should not be empty');
+    }
+
+    const Constructor = constructors[type];
+    if (typeof Constructor != 'function') {
+      throw new Error('can`t find constructor');
+    }
+
+    return new Constructor(params);
+  }
+  
+  static constructors = {
+    Espresso,
+    Americano,
+  };
+}
+
+
+// concreteFacgtoryB module
+class TeaFactory {
+  static create(type, params) {
+    if (!type) {
+      throw new Error('type should not be empty');
+    }
+
+    const Constructor = TeaFactory.constructors[type];
+    if (typeof Constructor != 'function') {
+      throw new Error('can`t find constructor');
+    }
+
+    return new Constructor(params);
+  }
+
+  static constructors = {
+    Red,
+    Green,
+    Black,
+  };
+}
+
+
+// client mocule
+const CoffeeFactory = DrinkFactory.createFactory('CoffeeFactory');
+const americano = CoffeeFactory.create('Americano', { amound: 2, sugar: true });
+const espresso = CoffeeFactory.create('Espresso', { amound: 3, sugar: false });
+
+const TeaFactory = DrinkFactory.createFactory('TeaFactory');
+const redTea = TeaFactory.create('Red', { sugar: true });
+const greenTea = TeaFactory.create('Green', { sugar: false });
+const blackTeam = TeaFactory.create('Black', { amound: 3, sugar: true });
