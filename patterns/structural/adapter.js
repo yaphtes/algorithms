8fbcaa952053171cae2392038c4843cf25a35c1f
Konstantin Otpushchennikov <otpushchennikov.ks@gmail.com>
2019-01-05 02:23:40 +0300

    Add adapter pattern

---
+++ patterns/structural/adapter.js
@@ -0,0 +1,114 @@
+// service provider module
+class Http {
+  request(method, url, data, done) {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method.toUpperCase(), url);
+    xhr.send(data);
+    xhr.onload = function () {
+      if (this.status == 200) {
+        done(null, this.responseText);
+      } else {
+        done(this.statusText);
+      }
+    }
+  }
+}
+
+
+// service consumer module
+class Repository {
+  constructor(baseUrl, resource, httpClient) {
+    this.baseUrl = baseUrl;
+    this.resource = resource;
+    this.httpClient = httpClient;
+  }
+
+  get url() {
+    return `${baseUrl}/${resource}`;
+  }
+
+  getAll() {
+    return this.http.get(this.url);
+  }
+
+  getOne(id) {
+    return this.http.get(`${this.url}/${id}`);
+  }
+
+  create(data) {
+    return this.http.post(this.url, data);
+  }
+
+  update(id, data) {
+    return this.http.put(`${this.url}/${id}`, data);
+  }
+
+  delete(id) {
+    return this.http.delete(`${this.url}/${id}`);
+  }
+}
+
+
+// pattern module
+class HttpAdapter {
+  constructor(http) {
+    this.http = http;
+  }
+
+  get(url) {
+    return new Promise((resolve, reject) => {
+      this.http.request('GET', url, null, (err, res) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(res);
+        }
+      });
+    });
+  }
+
+  post(url, data) {
+    return new Promise((resolve, reject) => {
+      this.http.request('POST', url, data, (err, res) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(res);
+        }
+      });
+    });
+  }
+
+  put(url, data) {
+    return new Promise((resolve, reject) => {
+      this.http.request('PUT', url, data, (err, res) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(res);
+        }
+      });
+    });
+  }
+
+  delete(url) {
+    return new Promise((resolve, reject) => {
+      this.http.request('DELETE', url, null, (err, res) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(res);
+        }
+      });
+    });
+}}
+
+
+// client module
+const http = new Http();
+const httpAdapter = new HttpAdapter(http)
+const repo = new Repository('http://courses.com', 'course', httpAdapter);
+
+repo.getAll()
+  .then(courses => console.log(courses));
+  
