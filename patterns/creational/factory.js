d66c5fd8ad0236beef05f076016bab6964ba874f
Konstantin Otpushchennikov <otpushchennikov.ks@gmail.com>
2019-01-05 03:21:30 +0300

    Add description in factory pattern

--- patterns/creational/factory.js
+++ patterns/creational/factory.js
@@ -1,23 +1,28 @@
+// Factory (фабрика)
+// Избавляет клиент от привязки к конкретным классам
+// Позволяет сгруппировать похожие "категории" классов в одной фабрике (единое место создания объектов)
+// Упрощает добавление новых классов в проект (зависимость добавляется только в фабрику, а не в клиентские модули)
+// Не нужно использовать с небольшим количеством классов
+
+
 // pattern module
 class CoffeeFactory {
   static create(type, params) {
-    if (!type) throw new Error('type should not be empty');
+    if (!type) throw new Error('Type should not be empty');
     
     const Constructor = CoffeeFactory.constructors[type];
-    if (typeof constructor != 'function') {
-      throw new Error('can`t find constructor')
-    }
+    if (typeof constructor != 'function') throw new Error(`Invalid type: ${type}`);
 
     return new Constructor(params);
   }
 
   static constructors = {
     Americano,
-    Espresso
+    Espresso,
   }
 }
 
 
 // client module
-const americano = CoffeeFactory.create('Americano', { amound: 2, sugar: false });
-const expresso = CoffeeFactory.create('Espresso', { amound: 3, sugar: true });
+const americano = CoffeeFactory.create('Americano', { foo: 'bar' });
+const expresso = CoffeeFactory.create('Espresso', { foo: 'bar' });
